31ac7f2a2a944ab535554cfcdda3fdd707734a76
nlwng49@gmail.com <nlwng49@gmail.com>
2017-03-16 15:44:41 +0800

    commit file 20170316_15:44:41

--- gitbook/install.sh
+++ gitbook/install.sh
@@ -12,3 +12,26 @@ nvm install node
 git clone --recursive git://github.com/isaacs/npm.git
 cd npm
 node cli.js install npm -g
+
+安装gitbook
+npm install gitbook-cli -g
+
+安装python2.7
+sudo apt-get install python2.7-dev
+wget https://bootstrap.pypa.io/ez_setup.py
+sudo python2 ez_setup.py install
+sudo easy_install-2.7 pip
+
+安装calibre
+http://www.calibre-ebook.com/download_linux
+sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+
+#使用gitbook
+mkdir -p ~/gitbookworkspace/demo
+cd ~/gitbookworkspace/demo
+gitbook init
+
+#可以生成网页
+gitbook build
+#可以生成pdf文件
+gitbook pdf ~/gitbookworkspace/demo ~/gitbookworkspace/demo.pdf
